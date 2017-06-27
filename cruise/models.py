7394500e7c00272c4635037580c26d2114ab0cde
Space <space@471.no>
2017-06-27 14:01:41 +0200

    Added Org., Participant and UserData model drafts
    Added

--- cruise/models.py
+++ cruise/models.py
@@ -2,8 +2,32 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
-
+class UserData(models.Model):
+	user = OneToOneField(User, on_delete=models.CASCADE)
+	is_crew = models.BooleanField()
+	role = models.CharField(max_length=50)
+	phone_number = models.CharField(max_length=50)
+	
+	organization = models.ForeignKeymodels.ForeignKey(Organization, on_delete=models.SET_NULL, null=True)
+	
+	nationality = models.CharField(max_length=50)
+	date_of_birth = models.DateField()
+	identity_document_types = models.CharField(max_length=200)
+	
+class Participant(models.Model):
+	cruise = models.ForeignKey(Cruise, on_delete=models.CASCADE)
+	email = models.EmailField()
+	name = models.CharField(max_length=200)
+	
+	nationality = models.CharField(max_length=50)
+	date_of_birth = models.DateField()
+	identity_document_types = models.CharField(max_length=200)
+	
+class Organization(models.Model):
+	name = models.CharField(max_length=200)
+	is_NTNU = models.BooleanField()
+	default_invoice_information = models.ForeignKey(InvoiceInformation, on_delete=models.SET_NULL, null=True)
+	
 class Cruise(models.Model):
 	cruise_name = models.CharField(max_length=200)
 	cruise_description = models.CharField(max_length=1000)
@@ -12,6 +36,10 @@ class Cruise(models.Model):
 	cruise_approved = models.BooleanField()
 	last_edit_date = models.DateTimeField(max_length=200)
 	submit_date = models.DateTimeField(max_length=200)
+	
+	season = models.ForeignKey(Season, on_delete=models.SET_NULL, null=True)
+	cruise_owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+	organization = models.ForeignKey(Organization, on_delete=models.SET_NULL, null=True)
 	
 	cruise_leader_name = models.CharField(max_length=200)
 	cruise_leader_email = models.EmailField()
@@ -36,3 +64,4 @@ class Cruise(models.Model):
 	was_edited_recently.admin_order_field = 'edit_date'
 	was_edited_recently.boolean = True
 	was_edited_recently.short_description = 'Edited recently?'
+
