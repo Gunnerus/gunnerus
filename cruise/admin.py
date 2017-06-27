300bc23b2e4d9ad95eaf4bb43c68c92d1bc46d8a
Space <space@471.no>
2017-06-27 15:24:19 +0200

    Added bunch of models + model name defs, registered models in admin.py

--- cruise/admin.py
+++ cruise/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from .models import Cruise
+from .models import Cruise, Event, InvoiceInformation, Organization, Season
 
-# Register your models here.
-
-admin.site.register(Cruise)
+admin.site.register([Cruise, Event, InvoiceInformation, Organization, Season])
--- cruise/models.py
+++ cruise/models.py
@@ -1,33 +1,25 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
-class UserData(models.Model):
-	user = OneToOneField(User, on_delete=models.CASCADE)
-	is_crew = models.BooleanField()
-	role = models.CharField(max_length=50)
-	phone_number = models.CharField(max_length=50)
-	
-	organization = models.ForeignKeymodels.ForeignKey(Organization, on_delete=models.SET_NULL, null=True)
-	
-	nationality = models.CharField(max_length=50)
-	date_of_birth = models.DateField()
-	identity_document_types = models.CharField(max_length=200)
-	
-class Participant(models.Model):
-	cruise = models.ForeignKey(Cruise, on_delete=models.CASCADE)
-	email = models.EmailField()
+MAX_PRICE_DIGITS = 12
+PRICE_DECIMAL_PLACES = 2 #decimals
+
+class Event(models.Model):
 	name = models.CharField(max_length=200)
 	
-	nationality = models.CharField(max_length=50)
-	date_of_birth = models.DateField()
-	identity_document_types = models.CharField(max_length=200)
+	def __str__(self):
+		return self.name
 	
-class Organization(models.Model):
-	name = models.CharField(max_length=200)
-	is_NTNU = models.BooleanField()
-	default_invoice_information = models.ForeignKey(InvoiceInformation, on_delete=models.SET_NULL, null=True)
+class TimeInterval(models.Model):
+	event = models.OneToOneField(Event, on_delete=models.CASCADE)
+	start_time = models.DateTimeField()
+	end_time = models.DateTimeField()
 	
+	def __str__(self):
+		return self.event.name
+
 class InvoiceInformation(models.Model):
 	business_reg_num = models.PositiveIntegerField()
 	invoice_address = models.CharField(max_length=200)
@@ -36,20 +28,58 @@ class InvoiceInformation(models.Model):
 	invoice_mark = models.CharField(max_length=200)
 	contact_name = models.CharField(max_length=200)
 	contact_email = models.EmailField()
-	
-class ListPrice(models.Model):
-	invoice = models.ForeignKeymodels.ForeignKey(InvoiceInformation, on_delete=models.CASCADE
+
+class Organization(models.Model):
 	name = models.CharField(max_length=200)
-	price = models.DecimalField()
+	is_NTNU = models.BooleanField()
+	default_invoice_information = models.ForeignKey(InvoiceInformation, on_delete=models.SET_NULL, null=True)
+
+	def __str__(self):
+		return self.name
+
+class UserData(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	is_crew = models.BooleanField()
+	role = models.CharField(max_length=50)
+	phone_number = models.CharField(max_length=50)
 	
+	organization = models.ForeignKey(Organization, on_delete=models.SET_NULL, null=True)
+	
+	nationality = models.CharField(max_length=50)
+	date_of_birth = models.DateField()
+	identity_document_types = models.CharField(max_length=200)
+	
+	def __str__(self):
+		return self.user.get_full_name()
+	
+class Season(models.Model):
+	name = models.CharField(max_length=100)
+	
+	season_interval = models.OneToOneField(TimeInterval, on_delete=models.SET_NULL, null=True, related_name='season_interval')
+	external_order_interval = models.OneToOneField(TimeInterval, on_delete=models.SET_NULL, null=True, related_name='external_order_interval')
+	internal_order_interval = models.OneToOneField(TimeInterval, on_delete=models.SET_NULL, null=True, related_name='internal_order_interval')
+	
+	long_education_price = models.DecimalField(max_digits=MAX_PRICE_DIGITS, decimal_places=PRICE_DECIMAL_PLACES)
+	long_research_price = models.DecimalField(max_digits=MAX_PRICE_DIGITS, decimal_places=PRICE_DECIMAL_PLACES)
+	long_boa_price = models.DecimalField(max_digits=MAX_PRICE_DIGITS, decimal_places=PRICE_DECIMAL_PLACES)
+	long_external_price = models.DecimalField(max_digits=MAX_PRICE_DIGITS, decimal_places=PRICE_DECIMAL_PLACES)
+	
+	short_education_price = models.DecimalField(max_digits=MAX_PRICE_DIGITS, decimal_places=PRICE_DECIMAL_PLACES)
+	short_research_price = models.DecimalField(max_digits=MAX_PRICE_DIGITS, decimal_places=PRICE_DECIMAL_PLACES)
+	short_boa_price = models.DecimalField(max_digits=MAX_PRICE_DIGITS, decimal_places=PRICE_DECIMAL_PLACES)
+	short_external_price = models.DecimalField(max_digits=MAX_PRICE_DIGITS, decimal_places=PRICE_DECIMAL_PLACES)
+	
+	def __str__(self):
+		return self.name
+
 class Cruise(models.Model):
 	cruise_name = models.CharField(max_length=200)
 	cruise_description = models.CharField(max_length=1000)
 	is_submitted = models.BooleanField()
 	terms_accepted = models.BooleanField()
 	cruise_approved = models.BooleanField()
-	last_edit_date = models.DateTimeField(max_length=200)
-	submit_date = models.DateTimeField(max_length=200)
+	last_edit_date = models.DateTimeField()
+	submit_date = models.DateTimeField()
 	
 	season = models.ForeignKey(Season, on_delete=models.SET_NULL, null=True)
 	cruise_owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
@@ -66,7 +96,7 @@ class Cruise(models.Model):
 	safety_clothing_and_equipment = models.CharField(max_length=200)
 	safety_analysis_requirements = models.CharField(max_length=200)
 	equipment_description = models.CharField(max_length=200)
-	meals_on_board = models.CharField(max_length=500)
+	meals_on_board = models.CharField(max_length=471)
 	
 	def __str__(self):
 		return self.cruise_name
@@ -78,4 +108,50 @@ class Cruise(models.Model):
 	was_edited_recently.admin_order_field = 'edit_date'
 	was_edited_recently.boolean = True
 	was_edited_recently.short_description = 'Edited recently?'
-
+	
+class Participant(models.Model):
+	cruise = models.ForeignKey(Cruise, on_delete=models.CASCADE)
+	email = models.EmailField()
+	name = models.CharField(max_length=200)
+	
+	nationality = models.CharField(max_length=50)
+	date_of_birth = models.DateField()
+	identity_document_types = models.CharField(max_length=200)
+	
+	def __str__(self):
+		return self.name
+	
+class CruiseDay(models.Model):
+	event = models.OneToOneField(Event, on_delete=models.SET_NULL, null=True)
+	cruise = models.OneToOneField(Cruise, on_delete=models.CASCADE)
+	
+	is_long_day = models.BooleanField()
+	description = models.CharField(max_length=471)
+	
+	breakfast_count = models.PositiveSmallIntegerField()
+	lunch_count = models.PositiveSmallIntegerField()
+	dinner_count = models.PositiveSmallIntegerField()
+	overnight_count = models.PositiveSmallIntegerField()
+	
+	def __str__(self):
+		return self.cruise.name
+	
+class GeographicalArea(models.Model):
+	cruise_day = models.OneToOneField(CruiseDay, on_delete=models.CASCADE)
+	name = models.CharField(max_length=200)
+	description = models.CharField(max_length=200)
+	
+	# lat/long is stored as decimal degrees.
+	latitude = models.DecimalField(max_digits=13, decimal_places=10)
+	longitude = models.DecimalField(max_digits=13, decimal_places=10)
+	
+	def __str__(self):
+		return self.name
+	
+class ListPrice(models.Model):
+	invoice = models.ForeignKey(InvoiceInformation, on_delete=models.CASCADE)
+	name = models.CharField(max_length=200)
+	price = models.DecimalField(max_digits=MAX_PRICE_DIGITS, decimal_places=PRICE_DECIMAL_PLACES)
+	
+	def __str__(self):
+		return self.name
