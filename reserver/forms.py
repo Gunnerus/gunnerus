609fa8dcc7d1d840661831599c6d42c1de87dbf7
Space <space@471.no>
2017-06-30 15:41:15 +0200

    Working on inline forms for CruiseDay and Participant

--- reserver/forms.py
+++ reserver/forms.py
@@ -1,18 +1,12 @@
 from django.db import models
 from django.forms import ModelForm, inlineformset_factory
 from reserver.models import Cruise, CruiseDay, Participant
-from extra_views import InlineFormSet
-from extra_views.generic import GenericInlineFormSet
 
 class CruiseForm(ModelForm):
 	class Meta:
 		model = Cruise
 		exclude = ('is_submitted','submit_date','last_edit_date')
 		
-class ParticipantInline(InlineFormSet):
-	fields = '__all__'
-	model = Participant
+CruiseDayFormSet = inlineformset_factory(Cruise, CruiseDay, fields='__all__', extra=1)
+ParticipantFormSet = inlineformset_factory(Cruise, Participant, fields='__all__', extra=1)
 
-class CruiseDayInline(InlineFormSet):
-	fields = '__all__'
-	model = CruiseDay
--- reserver/views.py
+++ reserver/views.py
@@ -4,15 +4,14 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
 
-from reserver.models import Cruise, UserData, TimeInterval, Event
-from reserver.forms import CruiseForm, CruiseDayInline, ParticipantInline
+from reserver.models import Cruise, UserData, Event
+from reserver.forms import CruiseForm, CruiseDayFormSet, ParticipantFormSet
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
-from extra_views import CreateWithInlinesView, UpdateWithInlinesView
 import datetime
 import json
 
@@ -20,15 +19,57 @@ class CruiseList(ListView):
 	model = Cruise
 	template_name = 'reserver/cruise_list.html'
 
-class CruiseCreateView(CreateWithInlinesView):
+class CruiseCreateView(CreateView):
 	model = Cruise
 	template_name = 'reserver/cruise_form.html'
 	fields = '__all__'
 	success_url = 'cruise-list'
-	inlines = [CruiseDayInline, ParticipantInline]
 	
 	def form_valid(self, form):
 		return super(CruiseCreateView, self).form_valid(form)
+		
+class TestCruiseCreateView(CreateView):
+    template_name = 'reserver/cruise_form.html'
+    model = Cruise
+    form_class = CruiseForm
+    success_url = 'cruise-list'
+
+    def get(self, request, *args, **kwargs):
+        self.object = None
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        cruiseday_form = CruiseDayFormSet()
+        participant_form = ParticipantFormSet()
+        return self.render_to_response(
+            self.get_context_data(form=form,
+                                  cruiseday_form=cruiseday_form,
+                                  participant_form=participant_form))
+
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        cruiseday_form = CruiseDayFormSet(self.request.POST)
+        participant_form = ParticipantFormSet(self.request.POST)
+        if (form.is_valid() and cruiseday_form.is_valid() and
+            participant_form.is_valid()):
+            return self.form_valid(form, cruiseday_form, participant_form)
+        else:
+            return self.form_invalid(form, cruiseday_form, participant_form)
+
+    def form_valid(self, form, cruiseday_form, participant_form):
+        self.object = form.save()
+        cruiseday_form.instance = self.object
+        cruiseday_form.save()
+        participant_form.instance = self.object
+        participant_form.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def form_invalid(self, form, cruiseday_form, participant_form):
+        return self.render_to_response(
+            self.get_context_data(form=form,
+                                  cruiseday_form=cruiseday_form,
+                                  participant_form=participant_form))
 
 class CruiseEditView(UpdateView):
 	model = Cruise
@@ -36,7 +77,7 @@ class CruiseEditView(UpdateView):
 	template_name = 'reserver/cruise_form.html'
 
 class CruiseDeleteView(DeleteView):
-	form = CruiseForm()
+	model = Cruise
 	template_name = 'reserver/cruise_form.html'
 	success_url = reverse_lazy('cruise-list')
 	
@@ -52,18 +93,18 @@ def login_view(request):
 	return render(request, 'reserver/login.html')
 	
 def signup_view(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'reserver/authform.html', {'form': form})
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			raw_password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=raw_password)
+			login(request, user)
+			return redirect('home')
+	else:
+		form = UserCreationForm()
+	return render(request, 'reserver/authform.html', {'form': form})
 	
 def register_view(request):
 	return render(request, 'reserver/register.html')
