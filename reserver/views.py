18a6a9d9feb2456ddc0eea80a18a3ca95e244609
Space <space@471.no>
2017-06-30 09:46:34 +0200

    Added login_required to views as appropriate

--- reserver/views.py
+++ reserver/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.views.generic import ListView
+from django.contrib.auth.decorators import login_required
 
 from reserver.models import Cruise
 from reserver.forms import CruiseForm
@@ -15,7 +16,8 @@ import json
 class CruiseList(ListView):
 	model = Cruise
 	template_name = 'reserver/cruise_list.html'
-	
+
+@login_required
 class CruiseCreateForm(CreateView):
 	form_class = CruiseForm
 	template_name = 'reserver/cruise_form.html'
@@ -24,12 +26,14 @@ class CruiseCreateForm(CreateView):
 	def form_valid(self, form):
 		#form.instance.cruise_owner = self.request.user
 		return super(CruiseCreateForm, self).form_valid(form)
-	
+
+@login_required
 class CruiseEditForm(UpdateView):
 	model = Cruise
 	fields = ('cruise_name',)
 	template_name = 'reserver/cruise_form.html'
-	
+
+@login_required
 class CruiseDeleteForm(DeleteView):
 	form = CruiseForm()
 	template_name = 'reserver/cruise_form.html'
@@ -37,10 +41,12 @@ class CruiseDeleteForm(DeleteView):
 	
 def index_view(request):
 	return render(request, 'reserver/index.html')
-	
+
+@login_required
 def admin_view(request):
 	cruises = get_list_or_404(Cruise)
 	return render(request, 'reserver/admin.html', {'cruises':cruises})
+
 def login_view(request):
 	return render(request, 'reserver/login.html')
 	
