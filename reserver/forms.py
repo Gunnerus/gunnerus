6629c04d9867728be1f646397d82edea490be297
Space <space@471.no>
2017-07-03 23:27:08 +0200

    Working on cruise form(set)

--- reserver/forms.py
+++ reserver/forms.py
@@ -7,6 +7,23 @@ class CruiseForm(ModelForm):
 		model = Cruise
 		exclude = ('is_submitted','submit_date','last_edit_date')
 		
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields['cruise_name'].help_text = "test"
+		
 CruiseDayFormSet = inlineformset_factory(Cruise, CruiseDay, fields='__all__', extra=1)
 ParticipantFormSet = inlineformset_factory(Cruise, Participant, fields='__all__', extra=1)
 
+class BetterCruiseForm(ModelForm):
+	class Meta:
+		model = Cruise
+		exclude = ('is_submitted','submit_date','last_edit_date')
+#
+#		self.fields['name'].widget.attrs.update({
+#					'placeholder': 'Name',
+#					'class': 'input-calss_name'
+#				})
+#		for field in iter(self.fields):
+#			self.fields[field].widget.attrs.update({
+#				'class': 'form-control'
+#		})
