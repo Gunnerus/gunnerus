5988b7c097ead29678ed2ae3bf75ed831fac7f54
Hallvard <hallvats@stud.ntnu.no>
2017-07-07 16:56:02 +0200

    Added UserData to django admin
    + småting i admin view

--- reserver/admin.py
+++ reserver/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Cruise, Event, InvoiceInformation, Organization, Season, CruiseDay, Participant
+from .models import Cruise, Event, InvoiceInformation, Organization, Season, CruiseDay, Participant, UserData
 
-admin.site.register([Cruise, Event, InvoiceInformation, Organization, Season, CruiseDay, Participant])
+admin.site.register([Cruise, Event, InvoiceInformation, Organization, Season, CruiseDay, Participant, UserData])
