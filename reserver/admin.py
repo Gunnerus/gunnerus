b867951b78ff99b41a9f61f52545405fe712148b
Hallvard <hallvats@stud.ntnu.no>
2017-07-07 17:11:58 +0200

    Userdata added to user in django admin

--- reserver/admin.py
+++ reserver/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from .models import Cruise, Event, InvoiceInformation, Organization, Season, CruiseDay, Participant, UserData
 
-admin.site.register([Cruise, Event, InvoiceInformation, Organization, Season, CruiseDay, Participant, UserData])
+class UserDataInline(admin.StackedInline):
+	model = UserData
+	can_delete = True
+	
+class UserAdmin(BaseUserAdmin):
+	inlines = (UserDataInline, )
+	
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+admin.site.register([Cruise, Event, InvoiceInformation, Organization, Season, CruiseDay, Participant])
