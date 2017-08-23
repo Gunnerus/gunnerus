f0d5be9f50db3c79ea33c11d7253aad7f67c1c51
Space <space@471.no>
2017-08-23 21:12:48 +0200

    Uncommenting init/startup model checks

--- reserver/apps.py
+++ reserver/apps.py
@@ -4,9 +4,9 @@ from reserver.utils import check_for_and_fix_users_without_userdata, check_defau
 class ReserverConfig(AppConfig):
 	name = 'reserver'
 	
-	def ready(self):
-		check_for_and_fix_users_without_userdata()
-		check_default_models()
+#	def ready(self):
+#		check_for_and_fix_users_without_userdata()
+#		check_default_models()
 
 #	def ready(self):
 #		from reserver import jobs
