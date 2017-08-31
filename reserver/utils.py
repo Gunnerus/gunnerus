66ef022dccd152abf8635962754de33465138634
Space <space@471.no>
2017-08-31 16:36:57 +0200

    Migrating emailtemplate groups

--- reserver/utils.py
+++ reserver/utils.py
@@ -4,8 +4,8 @@ from datetime import timedelta
 def init():
 	check_for_and_fix_users_without_userdata()
 	check_default_models()
-	#from reserver import jobs
-	#jobs.main()
+	from reserver import jobs
+	jobs.main()
 
 def check_for_and_fix_users_without_userdata():
 	from django.contrib.auth.models import User
@@ -97,10 +97,10 @@ def check_default_models():
 		other.save()
 	
 	# Check email templates
-#	from reserver.models import EmailTemplate
-#	for df in default_email_templates:
-#		try:
-#			template = EmailTemplate.objects.get(title=df[0])
-#		except EmailTemplate.DoesNotExist:
-#			template = EmailTemplate(title=df[0], group=df[1], message=df[2], time_before=df[3], date=df[4], is_active=df[5], is_muteable=df[6])
-#			template.save()
+	from reserver.models import EmailTemplate
+	for df in default_email_templates:
+		try:
+			template = EmailTemplate.objects.get(title=df[0])
+		except EmailTemplate.DoesNotExist:
+			template = EmailTemplate(title=df[0], group=df[1], message=df[2], time_before=df[3], date=df[4], is_active=df[5], is_muteable=df[6])
+			template.save()
