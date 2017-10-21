f8c9314b256bb2775b6b73b59b6eeaffbdbe22b5
Space <space@471.no>
2017-10-21 18:01:33 +0200

    rm a few newlines

--- gunnerus/settings.py
+++ gunnerus/settings.py
@@ -77,7 +77,6 @@ DEBUG_TOOLBAR_PANELS = [
 	'debug_toolbar.panels.profiling.ProfilingPanel'
 ]
 
-
 ROOT_URLCONF = 'gunnerus.urls'
 
 TEMPLATES = [
@@ -99,7 +98,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gunnerus.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -109,7 +107,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -129,7 +126,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -142,7 +138,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
