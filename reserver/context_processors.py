cfbafde32d6fb1920243ec80970ebc117fc06edb
Space <space@471.no>
2018-01-10 10:56:31 +0100

    moved admin link badges, add badge to pending invoices

--- reserver/context_processors.py
+++ reserver/context_processors.py
@@ -1,14 +1,16 @@
 def announcements_processor(request):
-	from reserver.models import Announcement, render_announcements
+	from reserver.models import Announcement, render_announcements, InvoiceInformation
 	from django.utils.safestring import mark_safe
 	from reserver.views import get_cruises_need_attention, get_users_not_approved, get_unapproved_cruises
+	from django.utils import timezone
 
 	if request.user.is_authenticated():
 		if request.user.is_superuser:
 			cruises_badge = len(get_cruises_need_attention())
 			users_badge = len(get_users_not_approved())
 			overview_badge = cruises_badge + users_badge + len(get_unapproved_cruises())
-			return {'announcements': mark_safe(render_announcements(user=request.user)), 'cruises_badge': cruises_badge, 'users_badge': users_badge, 'overview_badge': overview_badge}
+			unfinalized_invoices_badge = InvoiceInformation.objects.filter(is_finalized=False, cruise__is_approved=True, cruise__cruise_end__lte=timezone.now()).count()
+			return {'announcements': mark_safe(render_announcements(user=request.user)), 'cruises_badge': cruises_badge, 'users_badge': users_badge, 'overview_badge': overview_badge, 'unfinalized_invoices_badge': unfinalized_invoices_badge}
 		else:
 			return {'announcements': mark_safe(render_announcements(user=request.user))}
 	else:
