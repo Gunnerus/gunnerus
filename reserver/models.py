864a0eede13c17906eadade64090823e153fec2d
Unknown <hallvats@stud.ntnu.no>
2019-02-18 15:20:30 +0100

    Corrected invoice button visibilty
    Added new method on the InvoiceInformation model that checks if an invoice is done (is_paid, is_sent or is_finalized) and thus cannot be changed or deleted. Checks on is_finalized changed to is_done in case of discrepancy.

--- reserver/models.py
+++ reserver/models.py
@@ -826,7 +826,7 @@ class Cruise(models.Model):
 			invoice = self.get_invoice_info()
 
 			# do not update finalized/sent/paid invoices after the fact
-			if not (invoice.is_finalized or invoice.is_sent or invoice.is_paid):
+			if not (invoice.is_done()):
 				receipt = self.get_receipt()
 				invoice_items = ListPrice.objects.filter(invoice=invoice.pk, is_generated=True)
 
@@ -1059,6 +1059,12 @@ class InvoiceInformation(models.Model):
 		if self.cruise is None:
 			return True
 		return (self.cruise.cruise_end < timezone.now() and self.cruise.is_approved)
+
+	def is_done(self):
+		# checks whether the invoice is sent, paid or finalized - thus cannot be changed or deleted
+		if self.is_sent or self.is_paid or self.is_finalized:
+			return True
+		return False
 
 	def __str__(self):
 		if self.title != '':
